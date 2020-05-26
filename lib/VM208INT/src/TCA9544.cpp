3b3a52c95a70f85c63eaa5c6733fe2a5312a1e24
VELBN <8317833+velbn@users.noreply.github.com>
2020-05-26 15:48:56 +0200

    fix address bug TCA9544

--- lib/VM208INT/src/TCA9544.cpp
+++ lib/VM208INT/src/TCA9544.cpp
@@ -14,33 +14,33 @@ University of Miami
 #include <TCA9544.h>
 
 TCA9544::TCA9544(uint8_t addressBits){//feed in the A0-A2 bits to automatically adjust the TCA's address
-	deviceAddress = addressBits + 0x70;
-	controlRegisterState = 0; //The control register is zero on intialization
+	this->deviceAddress = addressBits;
+	this->controlRegisterState = 0; //The control register is zero on intialization
 }
 
 void TCA9544::changeChannel(uint8_t channelSelect){
-	controlRegisterState = ((controlRegisterState & 0b11110000) | (0b100 | channelSelect));
+	this->controlRegisterState = ((this->controlRegisterState & 0b11110000) | (0b100 | channelSelect));
 	Wire.beginTransmission(deviceAddress);
-	Wire.write(controlRegisterState);
+	Wire.write(this->controlRegisterState);
 	Wire.endTransmission();
 }
 
 uint8_t TCA9544::readInterrupts(){
 	uint8_t inters;
-	Wire.requestFrom(deviceAddress, uint8_t(1));
+	Wire.requestFrom(this->deviceAddress, uint8_t(1));
 	if (Wire.available()){
-		controlRegisterState = Wire.read();
+		this->controlRegisterState = Wire.read();
 	}
-	inters = controlRegisterState >> 4;
+	inters = this->controlRegisterState >> 4;
 	return inters;
 }
 
 uint8_t TCA9544::currentChannel(){
 	uint8_t channel;
-	Wire.requestFrom(deviceAddress, uint8_t(1));
+	Wire.requestFrom(this->deviceAddress, uint8_t(1));
 	if (Wire.available()){
-		controlRegisterState = Wire.read();
+		this->controlRegisterState = Wire.read();
 	}
-	channel = controlRegisterState & 0b11;
+	channel = this->controlRegisterState & 0b11;
 	return channel;
 }
