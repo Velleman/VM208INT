13b269d181d30d74994ae56d439cea6fb6eeacad
Brecht Nuyttens <bn@velleman.eu>
2020-06-10 15:23:57 +0200

    remove this->

--- src/VM208EXChannel.cpp
+++ src/VM208EXChannel.cpp
@@ -2,38 +2,38 @@
 
 VM208EXChannel::VM208EXChannel(uint8_t id, TCA6424A *tca)
 {
-    this->_id = id;
-    this->_tca = tca;
-    this->_reflectStatus = true;
+    _id = id;
+    _tca = tca;
+    _reflectStatus = true;
 }
 
 void VM208EXChannel::turnOn()
 {
-    this->_tca->writePin(this->_id, TCA6424A_HIGH);
-    if (this->_reflectStatus)
-        this->_tca->writePin(this->_id + TCA6424A_P20, TCA6424A_LOW);
+    _tca->writePin(_id, TCA6424A_HIGH);
+    if (_reflectStatus)
+        _tca->writePin(_id + TCA6424A_P20, TCA6424A_LOW);
 }
 
 void VM208EXChannel::turnOff()
 {
-    this->_tca->writePin(this->_id, TCA6424A_LOW);
-    if (this->_reflectStatus)
-        this->_tca->writePin(this->_id + TCA6424A_P20, TCA6424A_HIGH);
+    _tca->writePin(_id, TCA6424A_LOW);
+    if (_reflectStatus)
+        _tca->writePin(_id + TCA6424A_P20, TCA6424A_HIGH);
 }
 
 void VM208EXChannel::turnLedOn()
 {
-    this->_tca->writePin(this->_id + TCA6424A_P20, TCA6424A_LOW);
+    _tca->writePin(_id + TCA6424A_P20, TCA6424A_LOW);
 }
 
 void VM208EXChannel::turnLedOff()
 {
-    this->_tca->writePin(this->_id + TCA6424A_P20, TCA6424A_HIGH);
+    _tca->writePin(_id + TCA6424A_P20, TCA6424A_HIGH);
 }
 
 void VM208EXChannel::toggleLed()
 {
-    if(this->_tca->readPin(this->_id + TCA6424A_P20))
+    if(_tca->readPin(_id + TCA6424A_P20))
     {
         turnLedOff();
     }else{
@@ -43,21 +43,21 @@ void VM208EXChannel::toggleLed()
 
 bool VM208EXChannel::isButtonPressed()
 {
-    return !this->_tca->readPin(this->_id + TCA6424A_P10); //Active low so invert the result;
+    return !_tca->readPin(_id + TCA6424A_P10); //Active low so invert the result;
 }
 
 void VM208EXChannel::toggle()
 {
-    bool state = this->_tca->readPin(this->_id);
+    bool state = _tca->readPin(_id);
     if(state)
-        this->turnOff();
+        turnOff();
     else
     {
-        this->turnOn();
+        turnOn();
     }
 }
 
 void VM208EXChannel::reflectStatus(bool status)
 {
-    this->_reflectStatus = status;
+    _reflectStatus = status;
 }
