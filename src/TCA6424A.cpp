afb083091751cf25ba5854f208e229beb86994c6
Brecht Nuyttens <bn@velleman.eu>
2020-09-30 10:07:52 +0200

    Update TCA6424

--- src/TCA6424A.cpp
+++ src/TCA6424A.cpp
@@ -35,8 +35,16 @@ THE SOFTWARE.
 /** Default constructor, uses default I2C address.
  * @see TCA6424A_DEFAULT_ADDRESS
  */
-TCA6424A::TCA6424A() {
+TCA6424A::TCA6424A()
+{
     devAddr = TCA6424A_DEFAULT_ADDRESS;
+    registers[4] = 0xFF;
+    registers[5] = 0xFF;
+    registers[6] = 0xFF;
+
+    registers[0x0C] = 0xFF;
+    registers[0x0D] = 0xFF;
+    registers[0x0E] = 0xFF;
 }
 
 /** Specific address constructor.
@@ -45,8 +53,16 @@ TCA6424A::TCA6424A() {
  * @see TCA6424A_ADDRESS_ADDR_LOW
  * @see TCA6424A_ADDRESS_ADDR_HIGH
  */
-TCA6424A::TCA6424A(uint8_t address) {
+TCA6424A::TCA6424A(uint8_t address)
+{
     devAddr = address;
+    registers[4] = 0xFF;
+    registers[5] = 0xFF;
+    registers[6] = 0xFF;
+
+    registers[0x0C] = 0xFF;
+    registers[0x0D] = 0xFF;
+    registers[0x0E] = 0xFF;
 }
 
 /** Power on and prepare for general usage.
@@ -54,14 +70,16 @@ TCA6424A::TCA6424A(uint8_t address) {
  * will be default to INPUT mode, and the device is ready for usage immediately.
  * This is an empty function for consistency and/or future expansion.
  */
-void TCA6424A::initialize() {
+void TCA6424A::initialize()
+{
 }
 
 /** Verify the I2C connection.
  * Make sure the device is connected and responds as expected.
  * @return True if connection is valid, false otherwise
  */
-bool TCA6424A::testConnection() {
+bool TCA6424A::testConnection()
+{
     return I2Cdev::readBytes(devAddr, TCA6424A_RA_INPUT0, 3, buffer) == 3;
 }
 
@@ -70,23 +88,38 @@ bool TCA6424A::testConnection() {
 /** Get a single INPUT pin's logic level.
  * @return Pin logic level (0 or 1)
  */
-bool TCA6424A::readPin(uint16_t pin) {
-    I2Cdev::readBit(devAddr, TCA6424A_RA_INPUT0 + (pin / 8), pin % 8, buffer);
+bool TCA6424A::readPin(uint16_t pin)
+{
+    //I2Cdev::readBit(devAddr, TCA6424A_RA_INPUT0 + (pin / 8), pin % 8, buffer);
+    Wire.beginTransmission(devAddr);
+    Wire.write(TCA6424A_RA_INPUT0 + (pin / 8));
+    Wire.endTransmission();
+    Wire.requestFrom(devAddr, 1);
+    buffer[0] = 0;
+    buffer[0] = Wire.read();
+    buffer[0] = buffer[0] & (1 << pin % 8);
     return buffer[0];
 }
 /** Get all pin logic levels from one bank.
  * @param bank Which bank to read (0/1/2 for P0*, P1*, P2* respectively)
  * @return 8 pins' logic levels (0 or 1 for each pin)
  */
-uint8_t TCA6424A::readBank(uint8_t bank) {
-    I2Cdev::readByte(devAddr, TCA6424A_RA_INPUT0 + bank, buffer);
+uint8_t TCA6424A::readBank(uint8_t bank)
+{
+    //I2Cdev::readByte(devAddr, TCA6424A_RA_INPUT0 + bank, buffer);
+    Wire.beginTransmission(devAddr);
+    Wire.write(bank);
+    Wire.endTransmission();
+    Wire.requestFrom(devAddr, 1);
+    buffer[0] = Wire.read();
     return buffer[0];
 }
 /** Get all pin logic levels from all banks.
  * Reads into single 3-byte data container.
  * @param banks Container for all bank's pin values (P00-P27)
  */
-void TCA6424A::readAll(uint8_t *banks) {
+void TCA6424A::readAll(uint8_t *banks)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_INPUT0, 3, banks);
 }
 /** Get all pin logic levels from all banks.
@@ -95,7 +128,8 @@ void TCA6424A::readAll(uint8_t *banks) {
  * @param bank1 Container for Bank 1's pin values (P10-P17)
  * @param bank2 Container for Bank 2's pin values (P20-P27)
  */
-void TCA6424A::readAll(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2) {
+void TCA6424A::readAll(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_INPUT0, 3, buffer);
     *bank0 = buffer[0];
     *bank1 = buffer[1];
@@ -109,7 +143,8 @@ void TCA6424A::readAll(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2) {
  * necessarily represent the actual logic level present at the pin.
  * @return Pin output setting (0 or 1)
  */
-bool TCA6424A::getPinOutputLevel(uint16_t pin) {
+bool TCA6424A::getPinOutputLevel(uint16_t pin)
+{
     I2Cdev::readBit(devAddr, TCA6424A_RA_OUTPUT0 + (pin / 8), pin % 8, buffer);
     return buffer[0];
 }
@@ -119,7 +154,8 @@ bool TCA6424A::getPinOutputLevel(uint16_t pin) {
  * @param bank Which bank to read (0/1/2 for P0*, P1*, P2* respectively)
  * @return 8 pins' output settings (0 or 1 for each pin)
  */
-uint8_t TCA6424A::getBankOutputLevel(uint8_t bank) {
+uint8_t TCA6424A::getBankOutputLevel(uint8_t bank)
+{
     I2Cdev::readByte(devAddr, TCA6424A_RA_OUTPUT0 + bank, buffer);
     return buffer[0];
 }
@@ -127,7 +163,8 @@ uint8_t TCA6424A::getBankOutputLevel(uint8_t bank) {
  * Reads into single 3-byte data container.
  * @param banks Container for all bank's pin values (P00-P27)
  */
-void TCA6424A::getAllOutputLevel(uint8_t *banks) {
+void TCA6424A::getAllOutputLevel(uint8_t *banks)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_OUTPUT0, 3, banks);
 }
 /** Get all pin output settings from all banks.
@@ -138,7 +175,8 @@ void TCA6424A::getAllOutputLevel(uint8_t *banks) {
  * @param bank1 Container for Bank 1's pin values (P10-P17)
  * @param bank2 Container for Bank 2's pin values (P20-P27)
  */
-void TCA6424A::getAllOutputLevel(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2) {
+void TCA6424A::getAllOutputLevel(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_OUTPUT0, 3, buffer);
     *bank0 = buffer[0];
     *bank1 = buffer[1];
@@ -148,20 +186,38 @@ void TCA6424A::getAllOutputLevel(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2)
  * @param pin Which pin to write (0-23)
  * @param value New pin output logic level (0 or 1)
  */
-void TCA6424A::writePin(uint16_t pin, bool value) {
-    I2Cdev::writeBit(devAddr, TCA6424A_RA_OUTPUT0 + (pin / 8), pin % 8, value);
+void TCA6424A::writePin(uint16_t pin, bool value)
+{
+    //I2Cdev::writeBit(devAddr, TCA6424A_RA_OUTPUT0 + (pin / 8), pin % 8, value);
+    uint8_t pinNumber = pin % 8;
+    uint8_t reg = TCA6424A_RA_OUTPUT0 + (pin / 8);
+    if (value)
+    {
+        registers[reg] |= (0x01 << pinNumber);
+    }
+    else
+    {
+        registers[reg] &= ~(0x01 << pinNumber);
+    }
+    //I2Cdev::writeByte(devAddr, TCA6424A_RA_OUTPUT0 + (pin / 8), registers[reg]);
+    Wire.beginTransmission(devAddr);
+    Wire.write(TCA6424A_RA_OUTPUT0 + (pin / 8));
+    Wire.write(registers[reg]);
+    Wire.endTransmission();
 }
 /** Set all OUTPUT pins' logic levels in one bank.
  * @param bank Which bank to write (0/1/2 for P0*, P1*, P2* respectively)
  * @param value New pins' output logic level (0 or 1 for each pin)
  */
-void TCA6424A::writeBank(uint8_t bank, uint8_t value) {
+void TCA6424A::writeBank(uint8_t bank, uint8_t value)
+{
     I2Cdev::writeByte(devAddr, TCA6424A_RA_OUTPUT0 + bank, value);
 }
 /** Set all OUTPUT pins' logic levels in all banks.
  * @param banks All pins' new logic values (P00-P27) in 3-byte array
  */
-void TCA6424A::writeAll(uint8_t *banks) {
+void TCA6424A::writeAll(uint8_t *banks)
+{
     I2Cdev::writeBytes(devAddr, TCA6424A_RA_OUTPUT0 | TCA6424A_AUTO_INCREMENT, 3, banks);
 }
 /** Set all OUTPUT pins' logic levels in all banks.
@@ -169,7 +225,8 @@ void TCA6424A::writeAll(uint8_t *banks) {
  * @param bank1 Bank 1's new logic values (P10-P17)
  * @param bank2 Bank 2's new logic values (P20-P27)
  */
-void TCA6424A::writeAll(uint8_t bank0, uint8_t bank1, uint8_t bank2) {
+void TCA6424A::writeAll(uint8_t bank0, uint8_t bank1, uint8_t bank2)
+{
     buffer[0] = bank0;
     buffer[1] = bank1;
     buffer[2] = bank2;
@@ -181,7 +238,8 @@ void TCA6424A::writeAll(uint8_t bank0, uint8_t bank1, uint8_t bank2) {
 /** Get a single pin's polarity (normal/inverted) setting.
  * @return Pin polarity setting (0 or 1)
  */
-bool TCA6424A::getPinPolarity(uint16_t pin) {
+bool TCA6424A::getPinPolarity(uint16_t pin)
+{
     I2Cdev::readBit(devAddr, TCA6424A_RA_POLARITY0 + (pin / 8), pin % 8, buffer);
     return buffer[0];
 }
@@ -189,7 +247,8 @@ bool TCA6424A::getPinPolarity(uint16_t pin) {
  * @param bank Which bank to read (0/1/2 for P0*, P1*, P2* respectively)
  * @return 8 pins' polarity settings (0 or 1 for each pin)
  */
-uint8_t TCA6424A::getBankPolarity(uint8_t bank) {
+uint8_t TCA6424A::getBankPolarity(uint8_t bank)
+{
     I2Cdev::readByte(devAddr, TCA6424A_RA_POLARITY0 + bank, buffer);
     return buffer[0];
 }
@@ -197,7 +256,8 @@ uint8_t TCA6424A::getBankPolarity(uint8_t bank) {
  * Reads into single 3-byte data container.
  * @param banks Container for all bank's pin values (P00-P27)
  */
-void TCA6424A::getAllPolarity(uint8_t *banks) {
+void TCA6424A::getAllPolarity(uint8_t *banks)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_POLARITY0, 3, banks);
 }
 /** Get all pin polarity (normal/inverted) settings from all banks.
@@ -206,7 +266,8 @@ void TCA6424A::getAllPolarity(uint8_t *banks) {
  * @param bank1 Container for Bank 1's pin values (P10-P17)
  * @param bank2 Container for Bank 2's pin values (P20-P27)
  */
-void TCA6424A::getAllPolarity(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2) {
+void TCA6424A::getAllPolarity(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_POLARITY0, 3, buffer);
     *bank0 = buffer[0];
     *bank1 = buffer[1];
@@ -216,20 +277,23 @@ void TCA6424A::getAllPolarity(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2) {
  * @param pin Which pin to write (0-23)
  * @param polarity New pin polarity setting (0 or 1)
  */
-void TCA6424A::setPinPolarity(uint16_t pin, bool polarity) {
+void TCA6424A::setPinPolarity(uint16_t pin, bool polarity)
+{
     I2Cdev::writeBit(devAddr, TCA6424A_RA_POLARITY0 + (pin / 8), pin % 8, polarity);
 }
 /** Set all pin polarity (normal/inverted) settings in one bank.
  * @param bank Which bank to write (0/1/2 for P0*, P1*, P2* respectively)
  * @return New pins' polarity settings (0 or 1 for each pin)
  */
-void TCA6424A::setBankPolarity(uint8_t bank, uint8_t polarity) {
+void TCA6424A::setBankPolarity(uint8_t bank, uint8_t polarity)
+{
     I2Cdev::writeByte(devAddr, TCA6424A_RA_POLARITY0 + bank, polarity);
 }
 /** Set all pin polarity (normal/inverted) settings in all banks.
  * @param banks All pins' new logic values (P00-P27) in 3-byte array
  */
-void TCA6424A::setAllPolarity(uint8_t *banks) {
+void TCA6424A::setAllPolarity(uint8_t *banks)
+{
     I2Cdev::writeBytes(devAddr, TCA6424A_RA_POLARITY0 | TCA6424A_AUTO_INCREMENT, 3, banks);
 }
 /** Set all pin polarity (normal/inverted) settings in all banks.
@@ -237,7 +301,8 @@ void TCA6424A::setAllPolarity(uint8_t *banks) {
  * @param bank1 Bank 1's new polarity values (P10-P17)
  * @param bank2 Bank 2's new polarity values (P20-P27)
  */
-void TCA6424A::setAllPolarity(uint8_t bank0, uint8_t bank1, uint8_t bank2) {
+void TCA6424A::setAllPolarity(uint8_t bank0, uint8_t bank1, uint8_t bank2)
+{
     buffer[0] = bank0;
     buffer[1] = bank1;
     buffer[2] = bank2;
@@ -249,7 +314,8 @@ void TCA6424A::setAllPolarity(uint8_t bank0, uint8_t bank1, uint8_t bank2) {
 /** Get a single pin's direction (I/O) setting.
  * @return Pin direction setting (0 or 1)
  */
-bool TCA6424A::getPinDirection(uint16_t pin) {
+bool TCA6424A::getPinDirection(uint16_t pin)
+{
     I2Cdev::readBit(devAddr, TCA6424A_RA_CONFIG0 + (pin / 8), pin % 8, buffer);
     return buffer[0];
 }
@@ -257,7 +323,8 @@ bool TCA6424A::getPinDirection(uint16_t pin) {
  * @param bank Which bank to read (0/1/2 for P0*, P1*, P2* respectively)
  * @return 8 pins' direction settings (0 or 1 for each pin)
  */
-uint8_t TCA6424A::getBankDirection(uint8_t bank) {
+uint8_t TCA6424A::getBankDirection(uint8_t bank)
+{
     I2Cdev::readByte(devAddr, TCA6424A_RA_CONFIG0 + bank, buffer);
     return buffer[0];
 }
@@ -265,7 +332,8 @@ uint8_t TCA6424A::getBankDirection(uint8_t bank) {
  * Reads into single 3-byte data container.
  * @param banks Container for all bank's pin values (P00-P27)
  */
-void TCA6424A::getAllDirection(uint8_t *banks) {
+void TCA6424A::getAllDirection(uint8_t *banks)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_CONFIG0, 3, banks);
 }
 /** Get all pin direction (I/O) settings from all banks.
@@ -274,7 +342,8 @@ void TCA6424A::getAllDirection(uint8_t *banks) {
  * @param bank1 Container for Bank 1's pin values (P10-P17)
  * @param bank2 Container for Bank 2's pin values (P20-P27)
  */
-void TCA6424A::getAllDirection(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2) {
+void TCA6424A::getAllDirection(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2)
+{
     I2Cdev::readBytes(devAddr, TCA6424A_RA_CONFIG0, 3, buffer);
     *bank0 = buffer[0];
     *bank1 = buffer[1];
@@ -284,20 +353,38 @@ void TCA6424A::getAllDirection(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2) {
  * @param pin Which pin to write (0-23)
  * @param direction Pin direction setting (0 or 1)
  */
-void TCA6424A::setPinDirection(uint16_t pin, bool direction) {
-    I2Cdev::writeBit(devAddr, TCA6424A_RA_CONFIG0 + (pin / 8), pin % 8, direction);
+void TCA6424A::setPinDirection(uint16_t pin, bool direction)
+{
+    //I2Cdev::writeBit(devAddr, TCA6424A_RA_CONFIG0 + (pin / 8), pin % 8, direction);
+    uint8_t pinNumber = pin % 8;
+    uint8_t reg = TCA6424A_RA_CONFIG0 + (pin / 8);
+    if (direction)
+    {
+        registers[reg] |= (0x01 << pinNumber);
+    }
+    else
+    {
+        registers[reg] &= ~(0x01 << pinNumber);
+    }
+    //I2Cdev::writeByte(devAddr, reg, registers[reg]);
+    Wire.beginTransmission(devAddr);
+    Wire.write(reg);
+    Wire.write(registers[reg]);
+    Wire.endTransmission();
 }
 /** Set all pin direction (I/O) settings in one bank.
  * @param bank Which bank to read (0/1/2 for P0*, P1*, P2* respectively)
  * @param direction New pins' direction settings (0 or 1 for each pin)
  */
-void TCA6424A::setBankDirection(uint8_t bank, uint8_t direction) {
+void TCA6424A::setBankDirection(uint8_t bank, uint8_t direction)
+{
     I2Cdev::writeByte(devAddr, TCA6424A_RA_CONFIG0 + bank, direction);
 }
 /** Set all pin direction (I/O) settings in all banks.
  * @param banks All pins' new direction values (P00-P27) in 3-byte array
  */
-void TCA6424A::setAllDirection(uint8_t *banks) {
+void TCA6424A::setAllDirection(uint8_t *banks)
+{
     I2Cdev::writeBytes(devAddr, TCA6424A_RA_CONFIG0 | TCA6424A_AUTO_INCREMENT, 3, banks);
 }
 /** Set all pin direction (I/O) settings in all banks.
@@ -305,7 +392,8 @@ void TCA6424A::setAllDirection(uint8_t *banks) {
  * @param bank1 Bank 1's new direction values (P10-P17)
  * @param bank2 Bank 2's new direction values (P20-P27)
  */
-void TCA6424A::setAllDirection(uint8_t bank0, uint8_t bank1, uint8_t bank2) {
+void TCA6424A::setAllDirection(uint8_t bank0, uint8_t bank1, uint8_t bank2)
+{
     buffer[0] = bank0;
     buffer[1] = bank1;
     buffer[2] = bank2;
--- src/TCA6424A.h
+++ src/TCA6424A.h
@@ -35,106 +35,108 @@ THE SOFTWARE.
 
 #include "I2Cdev.h"
 
-#define TCA6424A_ADDRESS_ADDR_LOW   0x22 // address pin low (GND)
-#define TCA6424A_ADDRESS_ADDR_HIGH  0x23 // address pin high (VCC)
-#define TCA6424A_DEFAULT_ADDRESS    TCA6424A_ADDRESS_ADDR_LOW
+#define TCA6424A_ADDRESS_ADDR_LOW 0x22  // address pin low (GND)
+#define TCA6424A_ADDRESS_ADDR_HIGH 0x23 // address pin high (VCC)
+#define TCA6424A_DEFAULT_ADDRESS TCA6424A_ADDRESS_ADDR_LOW
 
-#define TCA6424A_RA_INPUT0          0x00
-#define TCA6424A_RA_INPUT1          0x01
-#define TCA6424A_RA_INPUT2          0x02
-#define TCA6424A_RA_OUTPUT0         0x04
-#define TCA6424A_RA_OUTPUT1         0x05
-#define TCA6424A_RA_OUTPUT2         0x06
-#define TCA6424A_RA_POLARITY0       0x08
-#define TCA6424A_RA_POLARITY1       0x09
-#define TCA6424A_RA_POLARITY2       0x0A
-#define TCA6424A_RA_CONFIG0         0x0C
-#define TCA6424A_RA_CONFIG1         0x0D
-#define TCA6424A_RA_CONFIG2         0x0E
+#define TCA6424A_RA_INPUT0 0x00
+#define TCA6424A_RA_INPUT1 0x01
+#define TCA6424A_RA_INPUT2 0x02
+#define TCA6424A_RA_OUTPUT0 0x04
+#define TCA6424A_RA_OUTPUT1 0x05
+#define TCA6424A_RA_OUTPUT2 0x06
+#define TCA6424A_RA_POLARITY0 0x08
+#define TCA6424A_RA_POLARITY1 0x09
+#define TCA6424A_RA_POLARITY2 0x0A
+#define TCA6424A_RA_CONFIG0 0x0C
+#define TCA6424A_RA_CONFIG1 0x0D
+#define TCA6424A_RA_CONFIG2 0x0E
 
-#define TCA6424A_AUTO_INCREMENT     0x80
+#define TCA6424A_AUTO_INCREMENT 0x80
 
-#define TCA6424A_LOW                0
-#define TCA6424A_HIGH               1
+#define TCA6424A_LOW 0
+#define TCA6424A_HIGH 1
 
-#define TCA6424A_POLARITY_NORMAL    0
-#define TCA6424A_POLARITY_INVERTED  1
+#define TCA6424A_POLARITY_NORMAL 0
+#define TCA6424A_POLARITY_INVERTED 1
 
-#define TCA6424A_OUTPUT             0
-#define TCA6424A_INPUT              1
+#define TCA6424A_OUTPUT 0
+#define TCA6424A_INPUT 1
 
-#define TCA6424A_P00                0
-#define TCA6424A_P01                1
-#define TCA6424A_P02                2
-#define TCA6424A_P03                3
-#define TCA6424A_P04                4
-#define TCA6424A_P05                5
-#define TCA6424A_P06                6
-#define TCA6424A_P07                7
-#define TCA6424A_P10                8
-#define TCA6424A_P11                9
-#define TCA6424A_P12                10
-#define TCA6424A_P13                11
-#define TCA6424A_P14                12
-#define TCA6424A_P15                13
-#define TCA6424A_P16                14
-#define TCA6424A_P17                15
-#define TCA6424A_P20                16
-#define TCA6424A_P21                17
-#define TCA6424A_P22                18
-#define TCA6424A_P23                19
-#define TCA6424A_P24                20
-#define TCA6424A_P25                21
-#define TCA6424A_P26                22
-#define TCA6424A_P27                23
+#define TCA6424A_P00 0
+#define TCA6424A_P01 1
+#define TCA6424A_P02 2
+#define TCA6424A_P03 3
+#define TCA6424A_P04 4
+#define TCA6424A_P05 5
+#define TCA6424A_P06 6
+#define TCA6424A_P07 7
+#define TCA6424A_P10 8
+#define TCA6424A_P11 9
+#define TCA6424A_P12 10
+#define TCA6424A_P13 11
+#define TCA6424A_P14 12
+#define TCA6424A_P15 13
+#define TCA6424A_P16 14
+#define TCA6424A_P17 15
+#define TCA6424A_P20 16
+#define TCA6424A_P21 17
+#define TCA6424A_P22 18
+#define TCA6424A_P23 19
+#define TCA6424A_P24 20
+#define TCA6424A_P25 21
+#define TCA6424A_P26 22
+#define TCA6424A_P27 23
 
-class TCA6424A {
-    public:
-        TCA6424A();
-        TCA6424A(uint8_t address);
-        
-        void initialize();
-        bool testConnection();
-        
-        // INPUT* registers (x0h - x2h)
-        bool readPin(uint16_t pin);
-        uint8_t readBank(uint8_t bank);
-        void readAll(uint8_t *banks);
-        void readAll(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
+class TCA6424A
+{
+public:
+    TCA6424A();
+    TCA6424A(uint8_t address);
 
-        // OUTPUT* registers (x4h - x6h)
-        bool getPinOutputLevel(uint16_t pin);
-        uint8_t getBankOutputLevel(uint8_t bank);
-        void getAllOutputLevel(uint8_t *banks);
-        void getAllOutputLevel(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
-        void writePin(uint16_t pin, bool polarity);
-        void writeBank(uint8_t bank, uint8_t value);
-        void writeAll(uint8_t *banks);
-        void writeAll(uint8_t bank0, uint8_t bank1, uint8_t bank2);
+    void initialize();
+    bool testConnection();
 
-        // POLARITY* registers (x8h - xAh)
-        bool getPinPolarity(uint16_t pin);
-        uint8_t getBankPolarity(uint8_t bank);
-        void getAllPolarity(uint8_t *banks);
-        void getAllPolarity(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
-        void setPinPolarity(uint16_t pin, bool polarity);
-        void setBankPolarity(uint8_t bank, uint8_t polarity);
-        void setAllPolarity(uint8_t *banks);
-        void setAllPolarity(uint8_t bank0, uint8_t bank1, uint8_t bank2);
+    // INPUT* registers (x0h - x2h)
+    bool readPin(uint16_t pin);
+    uint8_t readBank(uint8_t bank);
+    void readAll(uint8_t *banks);
+    void readAll(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
 
-        // CONFIG* registers (xCh - xEh)
-        bool getPinDirection(uint16_t pin);
-        uint8_t getBankDirection(uint8_t bank);
-        void getAllDirection(uint8_t *banks);
-        void getAllDirection(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
-        void setPinDirection(uint16_t pin, bool direction);
-        void setBankDirection(uint8_t bank, uint8_t direction);
-        void setAllDirection(uint8_t *banks);
-        void setAllDirection(uint8_t bank0, uint8_t bank1, uint8_t bank2);
+    // OUTPUT* registers (x4h - x6h)
+    bool getPinOutputLevel(uint16_t pin);
+    uint8_t getBankOutputLevel(uint8_t bank);
+    void getAllOutputLevel(uint8_t *banks);
+    void getAllOutputLevel(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
+    void writePin(uint16_t pin, bool polarity);
+    void writeBank(uint8_t bank, uint8_t value);
+    void writeAll(uint8_t *banks);
+    void writeAll(uint8_t bank0, uint8_t bank1, uint8_t bank2);
 
-    private:
-        uint8_t devAddr;
-        uint8_t buffer[3];
+    // POLARITY* registers (x8h - xAh)
+    bool getPinPolarity(uint16_t pin);
+    uint8_t getBankPolarity(uint8_t bank);
+    void getAllPolarity(uint8_t *banks);
+    void getAllPolarity(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
+    void setPinPolarity(uint16_t pin, bool polarity);
+    void setBankPolarity(uint8_t bank, uint8_t polarity);
+    void setAllPolarity(uint8_t *banks);
+    void setAllPolarity(uint8_t bank0, uint8_t bank1, uint8_t bank2);
+
+    // CONFIG* registers (xCh - xEh)
+    bool getPinDirection(uint16_t pin);
+    uint8_t getBankDirection(uint8_t bank);
+    void getAllDirection(uint8_t *banks);
+    void getAllDirection(uint8_t *bank0, uint8_t *bank1, uint8_t *bank2);
+    void setPinDirection(uint16_t pin, bool direction);
+    void setBankDirection(uint8_t bank, uint8_t direction);
+    void setAllDirection(uint8_t *banks);
+    void setAllDirection(uint8_t bank0, uint8_t bank1, uint8_t bank2);
+
+private:
+    uint8_t devAddr;
+    uint8_t buffer[3];
+    uint8_t registers[15];
 };
 
 #endif /* _TCA6424A_H_ */
